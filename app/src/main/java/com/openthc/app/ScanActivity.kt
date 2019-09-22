32ba0bc40e28404ed99601cbe233a395a68f54b4
edoceo <code@edoceo.com>
2019-09-22 00:24:04 -0700

    using newest scan tools

--- app/src/main/java/com/openthc/app/ScanActivity.kt
+++ app/src/main/java/com/openthc/app/ScanActivity.kt
@@ -5,47 +5,68 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
-import com.google.zxing.BarcodeFormat
+//import com.google.zxing.BarcodeFormat
 
-import kotlinx.android.synthetic.main.activity_auth_scan.*
+//import kotlinx.android.synthetic.main.activity_auth_scan.*
 import kotlinx.android.synthetic.main.content_auth_scan.*
-import com.google.zxing.Result
-import me.dm7.barcodescanner.zxing.ZXingScannerView
+//import com.google.zxing.Result
+//import me.dm7.barcodescanner.zxing.ZXingScannerView
 
-class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
+
+class ScanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth_scan)
-        setSupportActionBar(toolbar)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+		FirebaseApp.initializeApp(this)
 
-        qrCodeScanner.setFormats(listOf(BarcodeFormat.QR_CODE))
-        qrCodeScanner.setAutoFocus(true)
-        qrCodeScanner.setLaserColor(R.color.colorAccent)
-        qrCodeScanner.setMaskColor(R.color.colorAccent)
-        qrCodeScanner.startCamera()
-        qrCodeScanner.setResultHandler(this)
-
-
-        val Scan = BarcodeDetector.Builder(applicationContext)
-            .setBarcodeFormats(Barcode.ALL_FORMATS)
+        val options = FirebaseVisionBarcodeDetectorOptions.Builder()
+            .setBarcodeFormats(
+                FirebaseVisionBarcode.FORMAT_CODE_39,
+                FirebaseVisionBarcode.FORMAT_CODE_128,
+                FirebaseVisionBarcode.FORMAT_QR_CODE,
+                FirebaseVisionBarcode.FORMAT_PDF417
+            )
             .build()
+
+		val detector = FirebaseVision.getInstance()
+			.getVisionBarcodeDetector(options)
+
+        setContentView(R.layout.content_auth_scan)
+//        setContentView(R.layout.activity_auth)
+//        setSupportActionBar(toolbar)
+
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+//        val Scan = BarcodeDetector.Builder(applicationContext)
+//            .setBarcodeFormats(Barcode.ALL_FORMATS)
+//            .build()
+//
+////        Scan.setFormats(listOf(BarcodeFormat.QR_CODE))
+//        Scan.setAutoFocus(true)
+//        Scan.setLaserColor(R.color.colorAccent)
+//        Scan.setMaskColor(R.color.colorAccent)
+//        Scan.startCamera()
+//        Scan.setResultHandler(this)
 
     }
 
     /**
      * Handler for ZXing
      */
-    override fun handleResult(p0: Result?) {
-
-        if (p0 != null) {
-            // Attempt Login?
-
-            //startActivity(ScannedActivity.getScannedActivity(this, p0.text))
-            //resumeCamera()
-        }
-    }
+//    override fun handleResult(p0: Result?) {
+//
+//        if (p0 != null) {
+//            // Attempt Login?
+//
+//            //startActivity(ScannedActivity.getScannedActivity(this, p0.text))
+//            //resumeCamera()
+//        }
+//    }
 
 }
